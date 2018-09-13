220e2eb6d95975ebd8f8be5eed6db42a1302d296
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2018-09-12 22:43:58 -0300

    Create resolution for calculate percentage of used storage.

--- src/Storage.php
+++ src/Storage.php
@@ -2,10 +2,38 @@
 
 namespace Email;
 
+use Email\Models\Folders;
+
 class Storage
 {
-    public function calcFolderUsedPercent(): string
+    private $userId;
+
+    public function __construct($userId)
     {
-        return '0';
+        $this->userId = $userId;
+    }
+
+    public function calcFolderUsedPercent(Folders $folders): string
+    {
+        $conversion = 1024;
+        $storageGB = intval(str_replace('GB', '', $folders->getStorageSpace($this->userId)));
+        $userFolders = $folders->list($this->userId);
+        $spaceUsed = $this->spaceUsed($userFolders);
+
+        $storageMB = $storageGB * $conversion;
+        $percentUsed = ($spaceUsed * 100) / $storageMB;
+
+        return ceil($percentUsed) . '%';
+    }
+
+    private function spaceUsed(array $userFolders): int
+    {
+        $size = 0;
+
+        foreach ($userFolders as $folder) {
+            $size += intval(str_replace('MB', '', $folder->size));
+        }
+
+        return $size;
     }
 }
